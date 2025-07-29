570e6e7c9246b25b84776248b858e22fd3902fea
Yousef-ui-code <shabany204@gmail.com>
2025-07-29 22:33:57 +0300

    Add all CS50 Python projects

---
+++ interpreter/interpreter.py
@@ -0,0 +1,18 @@
+def main():
+    expression = input("Expression: ")
+    x, op, z = expression.split()
+    x = int(x)
+    z = int(z)
+
+    if op == "+":
+        result = x + z
+    elif op == "-":
+        result = x - z
+    elif op == "*":
+        result = x * z
+    elif op == "/":
+        result = x / z
+
+    print(f"{result:.1f}")
+
+main()
